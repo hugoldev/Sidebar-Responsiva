d89b115e0692361a32c701b2cc6d020c98463bb9
HugoLDev <hugolope30@icloud.com>
2023-03-20 19:02:22 -0300

    PerfectingSidebar

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
-function toggleMenu(){
-    const menuMobile = document.getElementById("menu-mobile")
+var header = document.getElementById('header')
+var element = document.getElementById('showSidebar')
+var main = document.getElementById('main')
+var showSidebar = false;
 
-    if(menuMobile.className === "menu-mobile-active"){
-        menuMobile.className = "menu-mobile"
+function toggleSidebar() {
+    showSidebar = !showSidebar;
+    console.log(showSidebar)
+    if(showSidebar){
+        element.style.marginLeft = '-1vw';
+        element.style.animationName = 'showSidebar';
+        main.style.filter = 'blur(1px)';
+    }
+    else {
+        element.style.marginLeft = '-100vw';
+        element.style.animationName = '';
+        main.style.filter = '';
+    }
+}
+
+function closeSidebar(){
+
+    if(showSidebar){
+        toggleSidebar();
     }
 
-    else {
-        menuMobile.className = "menu-mobile-active"
+}
+
+window.addEventListener('resize', function(event)) {
+    if(window.innerWidth > 574 && showSidebar){
+        toggleSidebar();
     }
 }
